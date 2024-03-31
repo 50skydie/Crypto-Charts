bdef662d1999840cc3affbb1799c7034c13670f1
Szymon Romanowski <36265196+50skydie@users.noreply.github.com>
2024-03-31 19:17:25 +0200

    starting prophet time series forecasting

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 import pandas as pd 
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime
+from sklearn.metrics import mean_squared_error
+import seaborn as sns
+import xgboost as xgb
+from prophet import Prophet
 
 
 def fetch_btc_price_for_x_days(x):    
@@ -43,7 +48,7 @@ def return_start_end_date(btc_price_array):
             datetime.utcfromtimestamp(btc_price_array[-1]['time']).strftime('%Y-%m-%d')]
 
 
-def plot_btc_mathplotlib(stock_prices):
+def plot_btc_candle(stock_prices):
     fig = plt.figure()
     up_prices = stock_prices[stock_prices.close >= stock_prices.open] 
     down_prices = stock_prices[stock_prices.close < stock_prices.open] 
@@ -57,25 +62,49 @@ def plot_btc_mathplotlib(stock_prices):
     plt.ylabel("USD", fontsize=16)
     plt.xlabel("Date", fontsize=16)
     plt.title(f"USD/BTC daily chart from {return_start_end_date(btc_price_history)[0]} to {return_start_end_date(btc_price_history)[1]}")
-    #plt.show()
+    plt.show()
 
-    canvas = FigureCanvasTkAgg(fig,master = window)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
-    toolbar = NavigationToolbar2Tk(canvas,window)
-    toolbar.update()
-    canvas.get_tk_widget().pack()
+    # canvas = FigureCanvasTkAgg(fig,master = window)
+    # canvas.draw()
+    # canvas.get_tk_widget().pack()
+    # toolbar = NavigationToolbar2Tk(canvas,window)
+    # toolbar.update()
+    # canvas.get_tk_widget().pack()
 
 
-btc_price_history = fetch_btc_price_for_x_days(7)
+btc_price_history = fetch_btc_price_for_x_days(333)
 stock_prices = convert_to_pandas(btc_price_history)
 
-window = Tk()
-window.title('Plotting in Tkinter')
-window.state('zoomed')   #zooms the screen to maxm whenever executed
-plot_button = Button(master = window,command = plot_btc_mathplotlib(stock_prices), height = 2, width = 10, text = "Plot")
-plot_button.pack()
-exit_button = Button(master = window, text="Quit", command=window.destroy)
-exit_button.pack()
-window.mainloop()
+prophet_train_model = stock_prices[['date', 'close']].rename(columns={'date' : 'ds', 'close' : 'y'})
+#print(pd.head())
+stock_prices = stock_prices[['date', 'close']].set_index('date')
+stock_prices.plot(figsize = (15,5))
+
+
+model = Prophet()
+model.fit(prophet_train_model)
+future_df = model.make_future_dataframe(periods=60)
+print(future_df)
+forecast = model.predict(future_df)
+#print(forecast)
+forecast_plt = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+model.plot(forecast_plt)
+plt.show()
+###
+### create model
+###
+
+#reg = xgb.XGBRegressor(n_estimators=31, early_stopping_round=50)
+#reg.fit(stock_prices['date'], stock_prices['close'], eval_set=[(stock_prices['date'], stock_prices['close'])], verbose=True)
+#plot_btc_candle(stock_prices)
+
+
+# window = Tk()
+# window.title('Plotting in Tkinter')
+# window.state('zoomed')   #zooms the screen to maxm whenever executed
+# plot_button = Button(master = window, command = plot_btc_candle(stock_prices), height = 2, width = 10, text = "Plot")
+# plot_button.pack()
+# exit_button = Button(master = window, text="Quit", command=window.destroy)
+# exit_button.pack()
+# window.mainloop()
  
